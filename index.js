f57a282baa5257fdea79f618e0b18556ecc53d0d
Gabriel Oliveira <gab.oliveira@outlook.com>
2015-10-15 22:40:57 -0300

    Bug fixes

--- index.js
+++ index.js
@@ -5,21 +5,17 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
-//app.configure(function () {
-//    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-//    app.use(express.bodyParser());
-//});
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/events', event.findAll);
-app.get('/event/:id', event.findById);
+app.get('/api/events', event.findAll);
+app.get('/api/events/:id', event.findById);
 
-app.get("/event/image/:id", event.getImage);
+app.get("/api/event/image/:id", event.getImage);
 
-app.get("/event/imagefile/:id", event.getImageFile);
+app.get("/api/event/imagefile/:id", event.getImageFile);
 
-app.post('/event', event.addEvent);
+app.post('/events', event.addEvent);
 app.put('/events/:id', event.updateEvent);
 app.delete('/events/:id', event.deleteEvent);
 
