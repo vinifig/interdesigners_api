346789190946044eecd1b94342a19952d0e026be
Gabriel Oliveira <gab.oliveira@outlook.com>
2015-10-15 21:07:27 -0300

    Bug fixes

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var express = require('express'),
     event = require('./lib/events');
+var cors = require("cors");
+var bodyParser = require("body-parser");
 
 var app = express();
 
-app.configure(function () {
-    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser());
-});
+//app.configure(function () {
+//    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+//    app.use(express.bodyParser());
+//});
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/events', event.findAll);
 app.get('/event/:id', event.findById);
